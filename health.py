00935a3d3a91f5f7231e7f635daec4a1125f8235
LinChiQ <111717457+LinChiQ@users.noreply.github.com>
2022-09-17 22:31:15 +0800

    Update health.py
    新增cookie保存本地读取功能

--- health.py
+++ health.py
@@ -14,7 +14,7 @@ def Login(headers, username, password):
         'username': username,
         'password': password
     }
-    login_req = session.post(login_url, params=params, headers=headers)
+    login_req = requests.post(login_url, params=params, headers=headers)
     text = json.loads(login_req.text)
     if text['code'] == 0:
         print(f"{username}账号登陆成功！")
@@ -23,6 +23,63 @@ def Login(headers, username, password):
     else:
         print(f"{username}登陆失败，请检查账号密码！")
         return False
+
+
+def ReadConfigData():
+    try:
+        with open('config.yml', 'r', encoding='utf-8') as f:
+            config = yaml.safe_load_all(f.read())
+            return config
+    except Exception as e:
+        print("读取错误", e)
+        exit(0)
+
+
+def GetMailConfig(config):
+    mail = yagmail.SMTP(config['mail'], config['password'], config['host'])
+    return mail
+
+
+def GetJWData():
+    try:
+        with open('jws.txt', 'r') as f:
+            jws_data = json.loads(f.read().replace("'", '"'))
+            print("读取现存jws文件成功！")
+            return jws_data
+    except FileNotFoundError:
+        print("jws文件不存在，正在创建！")
+        return {}
+    except Exception as e:
+        print("未知错误", e)
+        exit(0)
+
+
+def GetEach(config, data, headers):
+    try:
+        return data[config['username']]
+    except:
+        print(config['username'], "尝试登陆！")
+        login = Login(headers, config['username'], config['password'])
+        if login is False:
+            mail.send(config['receive'], '登陆失败', '登陆失败，请检查账号密码')
+            return False
+        data[str(config['username'])] = login
+        return login
+
+
+def testLoginStatus(headers, jws):
+    # 用任意需要鉴权的接口即可，这里随便选了一个
+    url = "https://student.wozaixiaoyuan.com/heat/getTodayHeatList.json"
+    headers['Host'] = "student.wozaixiaoyuan.com"
+    headers['JWSESSION'] = jws
+    res = requests.post(url, headers=headers)
+    text = json.loads(res.text)
+    if text['code'] == 0:
+        return True
+    elif text['code'] == -10:
+        return False
+    else:
+        return 0
 
 
 def GetSeq():
@@ -85,12 +142,14 @@ def Punch(headers, jws, address, health, *heat):
         sign_data['longitude'] = '102.863238'
     data = urlencode(sign_data)
     if heat[0][GetSeq() - 1] == 1:
-        heat_req = session.post(heat_url, data=data, headers=headers)
+        heat_req = requests.post(heat_url, data=data,
+                                 headers=headers)
         heat_text = json.loads(heat_req.text)
     if health == 1 and GetSeq() == 1:
         sign_data.pop('seq')
         data = urlencode(sign_data)
-        health_req = session.post(health_url, data=data, headers=headers)
+        health_req = requests.post(
+            health_url, data=data, headers=headers)
         health_text = json.loads(health_req.text)
     if heat_text:
         return heat_text
@@ -98,36 +157,42 @@ def Punch(headers, jws, address, health, *heat):
         return health_text
 
 
-def main(config):
+def main(config, data):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; TEL-AN10 Build/N6F26Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.117 Mobile Safari/537.36 MicroMessenger/8.0.25.2200(0x28001937) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android'
     }
-    success = Login(headers, config['username'], config['password'])
-    if not success:
-        user.send(config['receive'], '登陆失败', '登陆失败，请检查账号密码')
-        return False
+    jws = GetEach(config, data, headers)
+    if jws:
+        login_code = testLoginStatus(headers, jws)
+    else:
+        return
+    if login_code is False:
+        print(config['username'], "jws失效！")
+        jws = Login(headers, config['username'], config['password'])
+        if jws is False:
+            return False
+        print("jws文件更新成功")
+        data[config['username']] = jws
     try:
         punch_code = Punch(
-            headers, success, config['address'], config['health'], config['daily'])
+            headers, jws, config['address'], config['health'], config['daily'])
         if punch_code['code'] == 0:
-            user.send(config['receive'], '打卡成功', '打卡成功')
+            mail.send(config['receive'], '打卡成功', '打卡成功')
             print("打卡成功！")
             return True
         else:
-            user.send(config['receive'], '打卡失败', punch_code['message'])
+            mail.send(config['receive'], '打卡失败', punch_code['message'])
             print("打卡失败！", punch_code['message'])
             return False
-    except:
-        print("无需打卡！")
+    except Exception as e:
+        print("无需打卡！", e)
 
 
 if __name__ == '__main__':
-    session = requests.session()
-    with open('config.yml', 'r', encoding='utf-8') as f:
-        configs = yaml.safe_load_all(f.read())
-    mail_config = next(configs)
-    user = yagmail.SMTP(user=mail_config['mail'],
-                        password=mail_config['password'], host=mail_config['host'])
+    configs = ReadConfigData()
+    mail = GetMailConfig(next(configs))
+    jws_data = GetJWData()
     for config in configs:
-        main(config)
-    session.close()
+        main(config, jws_data)
+    with open('jws.txt', 'w') as f:
+        f.write(str(jws_data))
